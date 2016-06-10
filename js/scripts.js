85282865277eec1bdb2d2697bd9173a9201ae53d
Igor Seleznov <seleznov.igor@gmail.com>
2016-06-10 10:24:04 -0700

    add the template files and directories for css, fonts, img, js

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $("form#inputForm").submit(function() {
+    event.preventDefault();
+
+    var inputN = $("input#inputNumber").val();
+    var arrayOfN = [];
+    for (i = 0; i < inputN; i++) {
+      arrayOfN [i] = i + 1;
+    }
+    for  (var i = 1; i <= inputN; i++) {
+      if (arrayOfN [i] % 15 === 0){
+        arrayOfN [i] = "PING-PONG";
+      } else if (arrayOfN [i] % 5 === 0) {
+        arrayOfN [i] = "pong";
+      } else if (arrayOfN [i] % 3 === 0) {
+        arrayOfN [i] = "ping";
+      } else {
+      }
+    }
+    var indexNum = 0;
+    var unorderedList = document.getElementById('itemList');
+    var myElement;
+
+    function write_letters(){
+      for (i = 0; i < arrayOfN.length; i++ ) {
+        myElement = document.createElement("LI");
+        myElement.innerHTML = arrayOfN[indexNum++];
+        unorderedList.appendChild(myElement);
+      }
+    }
+    write_letters();
+  });
+});
